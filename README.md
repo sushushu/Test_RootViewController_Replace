65a4374a845f6fe23d21351e57b1e54f6ac71add
sushushu <jianzhimao@BatmanMac.local>
2020-06-03 17:27:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test_RootViewController_Replace
---
+++ Test_RootViewController_Replace/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  Test_RootViewController_Replace
+//
+//  Created by Jianzhimao on 2020/6/3.
+//  Copyright © 2020 Jianzhimao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
